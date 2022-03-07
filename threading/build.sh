c013b2897990554c24c17a2fd298dac6a82e8337
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2022-03-07 06:46:53 +0300

    added more interactive examples

---
+++ threading/build.sh
@@ -0,0 +1,20 @@
+echo "compiling..."
+
+PYTHON_VERSION=$(python3 --version | sed s/Python[[:space:]]//) # 3.9.5
+PYTHON_VER=${PYTHON_VERSION%.*}          # 3.9
+CFLAGS=`python3-config --cflags`
+LDFLAGS=`python3-config --ldflags`
+LDFLAGS+=" -lpython${PYTHON_VER} -lpthread"
+
+TARGETS="basic mutex1 join1"
+
+
+for fname in $TARGETS
+do
+    echo "compiling $fname"
+    clang -v $CFLAGS $LDFLAGS -lpython${PYTHON_VER} $fname.c -o $fname
+done
+
+echo "cleaning up..."
+rm -rf *.dSYM
+
